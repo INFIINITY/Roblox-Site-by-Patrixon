97943a14bb88213764d85bbf360a9b397461402c
Patrixon#3837 <87117669+INFIINITY@users.noreply.github.com>
2023-03-09 18:20:11 +0100

    Roblox Site Project by Patrixon

---
+++ Z&R.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="zdjecia/roblox.ico">
+    <link rel="stylesheet" href="style.css">
+    <title>Roblox</title>
+</head>
+<body>
+<body>
+  <div class="container">
+    <img src="zdjecia/logo.png" alt="roblox" class="logo">
+    <button class="rejestrPrzycisk">Zarejestruj się</button>
+    <button class="logPrzycisk">Zaloguj się</button>
+  </div>
+</body>
+</body>
+</html>
